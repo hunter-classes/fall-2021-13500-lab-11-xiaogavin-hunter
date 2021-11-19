a5ac4d72b051ea771c20af36d76f8d15c22d443d
xiaogavin-hunter <gavin.xiao89@myhunter.cuny.edu>
2021-11-19 13:06:41 -0500

    Need to add task headers

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 
 
 int main() { 
+    // Task A
     Profile p1("marco", "Marco");    
     std::cout << p1.getUsername() << std::endl; // marco
     std::cout << p1.getFullName() << std::endl; // Marco (@marco)
@@ -17,6 +18,7 @@ int main() {
     std::cout << p2.getUsername() << std::endl; // tarma1
     std::cout << p2.getFullName() << std::endl; // Tarma Roving (@tarma1)
 
+    // Task B
     Network nw;
     std::cout << nw.addUser("mario", "Mario") << std::endl;     // true (1)
     std::cout << nw.addUser("luigi", "Luigi") << std::endl;     // true (1)
@@ -30,5 +32,35 @@ int main() {
                     "Mario" + std::to_string(i)) << " ";   // true (1)
     std::cout << std::endl; 
     std::cout << nw.addUser("yoshi", "Yoshi") << std::endl;     // false (0)
+
+    // Task C
+    Network taskc;
+    // add three users
+    taskc.addUser("mario", "Mario");
+    taskc.addUser("luigi", "Luigi");
+    taskc.addUser("yoshi", "Yoshi");
+
+    // make them follow each other
+    taskc.follow("mario", "luigi");
+    taskc.follow("mario", "yoshi");
+    taskc.follow("luigi", "mario");
+    taskc.follow("luigi", "yoshi");
+    taskc.follow("yoshi", "mario");
+    taskc.follow("yoshi", "luigi");
+
+    // add a user who does not follow others
+    taskc.addUser("wario", "Wario");
+    
+    // add clone users who follow @mario
+    for(int i = 2; i < 6; i++) {
+        std::string usrn = "mario" + std::to_string(i);
+        std::string dspn = "Mario " + std::to_string(i);
+        taskc.addUser(usrn, dspn);
+        taskc.follow(usrn, "mario");
+    }
+    // additionally, make @mario2 follow @luigi
+    taskc.follow("mario2", "luigi");
+
+    taskc.printDot();
     return 0;
 }
